ec29025fbf9efe0c914f2d1bfc292fa5d39119d7
edxu96 <edxu96@outlook.com>
2019-06-20 15:49:00 +0200

    update

--- setup.py
+++ setup.py
@@ -4,21 +4,43 @@
 
 import sim_levy as sl
 import func_dist as fd
+import visualize as vi
 # import scipy.stats as stats
 # import matplotlib.pyplot as plt
+
+
+def task1():
+    mu = 1
+    sigma = 1
+    lamb = 1
+    n_sample = 100
+    vec_para = 1
+    func_sim_dist = lambda para: fd.exp(para)
+    n_sim = 100
+    list_p_result = [0] * n_sim
+    list_a_result = [0] * n_sim
+    list_m_result = [0] * n_sim
+    for i in range(n_sim):
+        list_p_result[i], list_a_result[i], list_m_result[i] = sl.sim(
+            n_sample, mu, sigma, lamb, func_sim_dist, vec_para)
+    vi.hist(list_p_result)
+    vi.hist(list_a_result)
+    vi.hist(list_m_result)
+    return list_p_result, list_a_result, list_m_result
 
 
 def test():
     mu = 1
     sigma = 1
     lamb = 1
-    num_sim = 100
+    n_sample = 100
     vec_para = 1
     func_sim_dist = lambda para: fd.exp(para)
-    p, a, m = sl.sim(num_sim, mu, sigma, lamb, func_sim_dist, vec_para)
+    p, a, m = sl.sim(n_sample, mu, sigma, lamb, func_sim_dist, vec_para)
     print("p = {}".format(p))
     print("a = {}".format(a))
     print("m = {}".format(m))
 
 
-test()
+# test()
+list_p_result, list_a_result, list_m_result = task1()
--- sim_levy.py
+++ sim_levy.py
@@ -16,27 +16,27 @@ def cal(a_pre, m_pre, y, phi1, phi2):
     return v, w, p, a, m
 
 
-def sim(num_sim, mu, sigma, lamb, func_sim_dist, vec_para):
+def sim(n_sample, mu, sigma, lamb, func_sim_dist, vec_para):
     # Calculate phi
     phi1 = mu / sigma ** 2 + np.sqrt(mu ** 2 / sigma ** 4 + 2 * lamb / sigma ** 2)
     phi2 = - mu / sigma ** 2 + np.sqrt(mu ** 2 / sigma ** 4 + 2 * lamb / sigma ** 2)
     # Pre-assign lists
-    list_v = [0] * num_sim
-    list_w = [0] * num_sim
-    list_p = [0] * num_sim
-    list_a = [0] * num_sim
-    list_m = [0] * num_sim
-    list_t = [0] * num_sim
-    list_y = [0] * num_sim
-    list_inter = [0] * num_sim
+    list_v = [0] * n_sample
+    list_w = [0] * n_sample
+    list_p = [0] * n_sample
+    list_a = [0] * n_sample
+    list_m = [0] * n_sample
+    list_t = [0] * n_sample
+    list_y = [0] * n_sample
+    list_inter = [0] * n_sample
     # Initialization
     list_y[0] = func_sim_dist(vec_para)
     list_p[0] = 0
     list_a[0] = 0
     list_m[0] = 0
-    for i in range(0, num_sim):
+    for i in range(0, n_sample):
         list_inter[i] = random.expovariate(lamb)
         list_t[i] = list_t[i - 1] + list_inter[i]
         list_y[i] = func_sim_dist(vec_para)
         list_v[i], list_w[i], list_p[i], list_a[i], list_m[i] = cal(list_a[i - 1], list_m[i - 1], list_y[i], phi1, phi2)
-    return list_p[num_sim - 1], list_a[num_sim - 1], list_m[num_sim - 1]
+    return list_p[n_sample - 1], list_a[n_sample - 1], list_m[n_sample - 1]
