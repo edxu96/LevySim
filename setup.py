96355496b7387c61392abff6607a67d65a418cac
edxu96 <edxu96@outlook.com>
2019-06-20 14:50:20 +0200

    update

--- setup.py
+++ setup.py
@@ -2,46 +2,43 @@
 # author: Edward J. Xu
 
 import numpy as np
-import scipy.stats as stats
+# import scipy.stats as stats
 import random
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
-def sim_levy(t, mu, sigma, aPre):
-    phi1 = mu / sigma^2 + np.sqrt(mu^2 / sigma^4 + 2 * lambda / sigma ^ 2)
-    phi2 = - mu / sigma^2 + np.sqrt(mu^2 / sigma^4 + 2 * lambda / sigma ^ 2)
+def sim_levy(mu, sigma, a_pre, m_pre, y, lamb):
+    phi1 = mu / sigma^2 + np.sqrt(mu^2 / sigma^4 + 2 * lamb / sigma ^ 2)
+    phi2 = - mu / sigma^2 + np.sqrt(mu^2 / sigma^4 + 2 * lamb / sigma ^ 2)
     v = random.expovariate(phi1)
     w = random.expovariate(phi2)
-    p = aPre + (v - w)
-    a = aPre + (v - w) + y
-    return v, w, x
-
-
-def sim_v(num_v):
-    list_v = [0] * num_v
-    for i in range(0, num_v):
-        t = random.expovariate(1 / lambda)
-        list_v[i] =
-
-
-
-def sim_w()
+    p = a_pre + (v - w)
+    a = a_pre + (v - w) + y
+    m = max(m_pre, a_pre + v, a_pre + (v - w) + y)
+    return v, w, p, a, m
 
 
 def test():
     mu = 0
     sigma = 0
-    lambda = 0
-    nSim = 100
-    phi1 = mu / sigma^2 + sqrt(mu^2 / sigma^4 + 2 * lambda / sigma^2)
-    phi2 = - mu / sigma^2 + sqrt(mu^2 / sigma^4 + 2 * lambda / sigma^2)
-
-
-
-    for i in range(0, nSim):
-        tInter = random.exp(lambda)
-        sim_levy(t, mu, sigma)
-
+    lamb = 0
+    num_sim = 100
+    # initialize
+    list_v = [0] * num_sim
+    list_w = [0] * num_sim
+    list_p = [0] * num_sim
+    list_a = [0] * num_sim
+    list_m = [0] * num_sim
+    list_t = [0] * num_sim
+    list_y = [0] * num_sim
+    list_inter = [0] * num_sim
+    for i in range(0, num_sim):
+        list_inter[i] = random.exp(lamb)
+        list_t[i] = list_t[i - 1] + list_inter[i]
+        list_y[i] = random.expovariate(1)
+        list_v[i], list_w[i], list_p[i], list_a[i], list_m[i] = sim_levy(mu, sigma, list_a[i - 1], list_m[i - 1],
+                                                                         list_y[i], lamb)
+    return list_p, list_a, list_m
 
 
 test()
