dff6b5785e8f8a2d3f679701c2e3888455da7492
Sen <43621786+SenZhan@users.noreply.github.com>
2019-06-21 10:28:25 +0200

    New

--- task1_Sen.py
+++ task1_Sen.py
@@ -1,50 +1,50 @@
 # task 1
-
-from random import random
-import scipy.stats as stats
 import matplotlib.pyplot as plt
 import numpy.random as npr
 import math
 
 nLoop = 1
-jump = 1000
+jump = 15
 
 lambda1 = 1
+lambda2 = 5
 mu = 0.5
 sigma = 1
 phi1 = mu/(sigma**2) + math.sqrt(mu**2/sigma**4+2*lambda1/sigma**2)
 phi2 = -mu/(sigma**2) + math.sqrt(mu**2/sigma**4+2*lambda1/sigma**2)
 
-A1k = [0] * nLoop
+A1k = [0] * nLoop  # number of values 
 M1k = [0] * nLoop
 P1k = [0] * nLoop   
 
-for ind in range(1,nLoop+1):
+for ind in range(nLoop):
     T = [0] * (jump+1)
     A = [0] * (jump+1)
     M = [0] * (jump+1)
     P = [0] * (jump+1) 
     V = [0] * (jump+1)
     W = [0] * (jump+1)    
-    Y = [0] * (jump+1)    
+    Y = [0] * (jump+1)   
+    
     for j in range(1, jump+1):
-        T[j] = T[j-1] + npr.exponential(lambda1)
-        V[j] = npr.exponential(phi1)
-        W[j] = npr.exponential(phi2)
+        T[j] = T[j-1] + npr.exponential(1/lambda1)
+        V[j] = npr.exponential(1/phi1)
+        W[j] = npr.exponential(1/phi2)
+        Y[j] = npr.exponential(1/lambda2)
         P[j] = A[j-1] + V[j] - W[j]
-        Y[j] = npr.exponential(1)
         A[j] = P[j] + Y[j]
         M[j] = max(M[j-1], A[j-1]+V[j], A[j])
         
-    A1k[ind-1] = A[jump]
-    P1k[ind-1] = P[jump]
-    M1k[ind-1] = M[jump]
+    A1k[ind] = A[jump]
+    P1k[ind] = P[jump]
+    M1k[ind] = M[jump]
     
-    plt.plot(T, A)
-    #plt.plot(T, [V[i]-W[i] for i in range(jump+1)])
+    # plt.plot(T, A)
+    # plt.plot(T, P)
+    plt.plot(T, M)
+    plt.xlabel('time')
+    plt.ylabel('best')
     plt.show()
-  
-
         
         
         
