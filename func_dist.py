886f344b49f67854fd7d5c0c0975ae1a9937b887
edxu96 <edxu96@outlook.com>
2019-06-21 10:51:40 +0200

    update

--- func_dist.py
+++ func_dist.py
@@ -5,6 +5,13 @@
 import random
 
 def exp(lamb):
+    """
+    Generate x from exponential distribution
+    pdf(x, lambda) 1 / beta * exp(- x / beta)
+    mean = lambda = 1 / beta
+    :param lamb:
+    :return:
+    """
     return random.expovariate(lamb)
 
 
