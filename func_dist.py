bc5952e6b9774c665d0bec2921ef6f1ebd6d0454
losdwind <s524256521@gmail.com>
2019-06-21 14:20:06 +0200

    Update func_dist.py

--- func_dist.py
+++ func_dist.py
@@ -2,9 +2,15 @@
 # author: Edward J. Xu
 # date: 190620
 
-import random
+import numpy as np
+import scipy.stats as st
 
 def exp(lamb):
-    return random.expovariate(lamb)
+    return np.random.exponential(1/lamb, size = 1)
+
+# rvs(a, loc=0, scale=1, size=1, random_state=None)
+    
+def erlang(shape):
+    return st.erlang(shape).rvs()
 
 
